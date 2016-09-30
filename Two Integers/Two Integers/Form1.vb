65d6f6c8e3d502549ea7c61a96c951281250eb6a
Nick-Nahnybida <nicknahnybida@gmail.com>
2016-09-30 10:28:08 -0500

    Add files via upload

---
+++ Two Integers/Two Integers/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+
+    Private Sub btnCompare_Click(sender As Object, e As EventArgs) Handles btnCompare.Click
+        Dim intA, intB As Integer
+
+        ' Declare variables from input
+        intA = CInt(txtIntA.Text)
+        intB = CInt(txtIntB.Text)
+
+        ' The brain of the program
+        If (intA) < (intB) Then
+            lblResult.Text = "Value B is greatest"
+
+        ElseIf intA > intB Then
+            lblResult.Text = "Value A is greatest"
+
+        ElseIf intA = intB Then
+            lblResult.Text = "Values are the same"
+        End If
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
